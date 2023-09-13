f836e929cd9b2f26f68eec6149d88d18effc9f4e
Josias Alves <81787907+josi-dev@users.noreply.github.com>
2023-09-13 09:21:30 -0300

    Add files via upload

---
+++ connection/PHP/connect.php
@@ -0,0 +1,18 @@
+<?php
+/*
+$host = "sql305.infinityfree.com";
+$user = "if0_34995105";
+$pass = "dgCU3z2dX8Q4t";
+$base = "if0_34995105_gerenciador_base";
+
+$host = "192.168.100.56";
+$user = "root";
+$pass = "root";
+$base = "teste";
+
+
+
+$conn = mysqli_connect($host,$user,$pass,$base);
+*/
+
+echo("conexao aquu");
